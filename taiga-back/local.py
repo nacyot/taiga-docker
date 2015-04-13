e3976d87fe62880806ecebc3cf88440f4d806119
nacyot <propellerheaven@gmail.com>
2015-04-13 17:47:45 +0900

    Set taiga ldap plugin configuration

--- taiga-back/local.py
+++ taiga-back/local.py
@@ -1,8 +1,12 @@
+import os
 from .common import *
-
 from .dockerenv import *
 
 INSTALLED_APPS += ["taiga_contrib_slack"]
+INSTALLED_APPS += ["taiga_contrib_ldap_auth"]
+LDAP_SERVER = os.getenv("LDAP_SERVER")
+LDAP_DN_FORMAT = os.getenv("LDAP_DN_FORMAT")
+LDAP_BASE_EMAIL = os.getenv("LDAP_BASE_EMAIL")
 
 # THROTTLING
 #REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"] = {
